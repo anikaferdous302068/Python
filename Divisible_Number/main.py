e8d69d18c302b18cb642f611f301699fb6ce8724
anikaferdous302068 <anika.shesc@gmail.com>
2026-01-09 10:57:41 +0600

    done

---
+++ Divisible_Number/main.py
@@ -0,0 +1,10 @@
+print ("Enter a numerator: ")
+numerator = int(input())
+print ("Enter a denominator: ")
+denominator = int(input())
+
+if numerator%denominator == 0:
+    print ("The numerator is divisible by the denominator.")
+else:
+    print ("The numerator is not divisible by the denominator.")
+    
