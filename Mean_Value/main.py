ea03e73340a57c08a29950db35ba47fc74d6323d
anikaferdous302068 <anika.shesc@gmail.com>
2026-01-09 11:10:00 +0600

    done

---
+++ Mean_Value/main.py
@@ -0,0 +1,13 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_numbers = 40
+sum = mean1 * total_numbers
+print("Sum of all numbers:", sum)
+
+num2 = sum- ((wrong_number) - (correct_number))
+print ("sum - ((wrong_number) - (correct_number)", num2)
+
+mean2 = num2 / total_numbers
+print (mean2)
+
