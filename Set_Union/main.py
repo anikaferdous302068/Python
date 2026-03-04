8fc3e76fa5d3bd302429473c68a409dc3602b9f6
anikaferdous302068 <anika.shesc@gmail.com>
2026-03-04 09:53:57 +0600

    done

---
+++ Set_Union/main.py
@@ -0,0 +1,8 @@
+setx={"green", "blue"}
+sety={"blue", "yellow"}
+print("Original set elements:")
+print("Set x:", setx)
+print("Set y:", sety)
+print("Union of two said sets:")
+setz=setx.union(sety)
+print("Set z:", setz)
