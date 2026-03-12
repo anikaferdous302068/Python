7b62e375fd5e29157f56adf08a158259aa7a2b40
anikaferdous302068 <anika.shesc@gmail.com>
2026-03-12 17:50:36 +0600

    done

---
+++ Class_Vehicle/main.py
@@ -0,0 +1,10 @@
+class Vehicle:
+    def__init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+modelX = Vehicle(240, 18)
+modelY = Vehicle(220, 16)
+print("Model X Max Speed:", modelX.max_speed, "km/h")
+print("Model Y Max Speed:", modelY.max_speed, "km/h")
+print("Model X Mileage:", modelX.mileage, "km/l")
+print("Model Y Mileage:", modelY.mileage, "km/l")
